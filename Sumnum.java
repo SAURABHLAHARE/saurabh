7676d3cd43bd1fef9e08df06e0ea1b70381f43c3
SAURABHLAHARE <saurabhlahare25@gmail.com>
2024-12-30 08:37:56 +0530

    Add files via upload

---
+++ Sumnum.java
@@ -0,0 +1,7 @@
+class Sumnum
+{
+public static void main (string args[]){
+System.out.println("add");
+int a=5,b=6;
+int c=a+b;
+}
---
+++ sum.java
@@ -0,0 +1,5 @@
+public class sum {
+    public static void main(String[] args) {
+        System.out.println("5 + 10 = " + (5 + 10));
+    }
+}
